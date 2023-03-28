14bfbf093e46b866d500c748b57f9e5726667cbd
ndittren <36773036+ndittren@users.noreply.github.com>
2023-03-28 15:31:34 -0400

    :bookmark: release 0.1.8

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup
 
 setup(
     name="logiclearnertools",
-    version="0.1.7",
+    version="0.1.8",
     author="Natalia Dittren, Columbia University Computer Science Faculty and Students",
     author_email="natalia.d@columbia.edu",
     url="https://github.com/ccnmtl/logiclearnertools",
